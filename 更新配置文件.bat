e668f7528e3052b2b8999724a59fd154bfbea0a2
jaove <3158953@qq.com>
2020-01-17 08:28:41 +0800

    Daily Update !

--- 更新配置文件.bat
+++ 更新配置文件.bat
@@ -1,10 +1,10 @@
 @echo off
+git pull
+
 python gethost-SS.py
 python ss2ssr.py -j ..\..\ShadowsocksR-win-4.9.2\gui-config.json
 python gethost-V2Ray.py
 
-
-git pull
 git add .
 git commit -m "Daily Update !"
 git push
