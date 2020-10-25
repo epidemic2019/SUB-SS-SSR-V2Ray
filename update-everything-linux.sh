a5d7961a20875e0068bb58744e57682beefb614c
root <root@CentOS.192.168.10.1>
2020-10-25 17:27:56 +0800

    Daily Update !

--- update-everything-linux.sh
+++ update-everything-linux.sh
@@ -1,10 +1,10 @@
 git pull
 
 ./gethost-SS.py
-./ss2ssr.py -j ..\..\ShadowsocksR-win-4.9.2\gui-config.json
-./gethost-V2Ray.py
-./mergefile.py
-./gethost-jj.py
+#./ss2ssr.py -j ..\..\ShadowsocksR-win-4.9.2\gui-config.json
+#./gethost-V2Ray.py
+#./mergefile.py
+#./gethost-jj.py
 
 git add .
 git commit -m "Daily Update !"
