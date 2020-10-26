a3608c32faecc19bbe5e60cb487f66c1bc79689f
jaove <3158953@qq.com>
2020-10-26 09:56:34 +0800

    Daily Update !

--- gethost-jj.py
+++ gethost-jj.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python
 import requests
+import base64
+import chardet
+
 url = 'https://jj-rss-01.best/link/CR0fY9iH5GntsUxi?sub=1'
 r = requests.get(url, allow_redirects=True)  # to get content after redirection
-pdf_url = r.url # 'https://media.readthedocs.org/pdf/django/latest/django.pdf'
-with open('base64_jj.txt', 'wb') as f:
-    f.write(r.content)
+
+if chardet.detect(base64.b64decode(r.content))['encoding']=='ascii':
+    str_line=base64.b64decode(r.content).decode().split('\n')
+    keys=['ss://','ssr://','vess://']
+    for value in keys:
+        if str_line[0].find(value)>=0:
+           with open('base64_jj.txt', 'wb') as f:
+               f.write(r.content)
+               f.close
+           print('\n订阅更新完成！')
+else:
+    print('\n订阅地址不可用或尝试关闭代理后更新！')
