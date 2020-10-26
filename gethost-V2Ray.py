ca5fd74008a9f58027a219df63ef98c509c9720b
root <root@PVE.lan>
2020-10-26 15:01:19 +0800

    Daily Update !

--- gethost-V2Ray.py
+++ gethost-V2Ray.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import base64
 import time
 import sys,os
--- gethost-jj.py
+++ gethost-jj.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import requests
 import base64
 import chardet
